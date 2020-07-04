b187947d2a052a5ae6c085ed3205d6f3d89e6372
monsterpit <monsterpit104@gmail.com>
2020-07-05 01:47:37 +0530

    playing a stored Video

--- Resources/chapter1.py
+++ Resources/chapter1.py
@@ -1,7 +1,15 @@
 import cv2
 print("package imported")
 
-img = cv2.imread("trump.png")
-cv2.imshow("Output", img)
+#For image
+#img = cv2.imread("trump.png")
+#cv2.imshow("Output", img)
+#cv2.waitKey(0)
 
-cv2.waitKey(0)
+cap = cv2.VideoCapture("nature.mp4")
+
+while True:
+    success,img = cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
