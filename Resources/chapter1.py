3fb7ca448beac1dfdb5b9245e2ea37989c37a9b8
monsterpit <monsterpit104@gmail.com>
2020-07-05 14:31:01 +0530

    joining images

--- Resources/chapter1.py
+++ Resources/chapter1.py
@@ -1,18 +1,54 @@
 import cv2
 import numpy as np
 
-###Warp perspective
-#To get bird eye view of image
+###Joining images
+#Lot of images in 1 window
 
-img = cv2.imread("cards.png")
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
-width,height = 250,300
-pt1 = np.float32([[44,356],[250,314],[71,669],[315,603]])
-pt2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 
+img = cv2.imread("trump.png")
 
-matrix = cv2.getPerspectiveTransform(pt1,pt2)
-imgOutput = cv2.warpPerspective(img,matrix,(width,height))
-cv2.imshow("Image",img)
-cv2.imshow("output Image",imgOutput)
+#numpy function
+#disadvantages of this method 1) we cant resize the image 2)if images do not have same number of channel which means they are nopt both rgb and may 1 of them is grey and 1 of them is rgb then it will not work both of them have to have same number of channelswe are talking about matrices
+#so we have function to tackle it
+hor = np.hstack((img,img))
+ver = np.vstack((img,img))
+
+grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+imgStack = stackImages(0.5,([img,grayImg,img],[img,img,img]))
+
+cv2.imshow("Horizontal",hor)
+cv2.imshow("Vertical",ver)
+cv2.imshow("Image Stack",imgStack)
 cv2.waitKey(0)
